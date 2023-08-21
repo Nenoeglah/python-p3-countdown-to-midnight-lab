b4a551240d0def9f1f5d70c13ce056c3cdb34a5a
Nenoeglah <eglah.chepngeno@student.moringaschool.com>
2023-08-21 15:48:35 +0300

    changed code on app.py

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,25 @@
 # your code goes here!
+def countdown(seconds):
+    while seconds > 0:
+        print(f'{seconds} SECOND(S)!')
+        seconds -= 1
+    print("HAPPY NEW YEAR!")
+countdown(10)
+
+import time
+
+def countdown(seconds):
+    while seconds > 0:
+        print(f'{seconds} SECOND(S)!')
+        seconds -= 1
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(seconds):
+    while seconds > 0:
+        print(f'{seconds} SECOND(S)!')
+        seconds -= 1
+        time.sleep(1)  
+    print("HAPPY NEW YEAR!")
+   
+    
+
